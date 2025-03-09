4f3eee5673ce8d96e9f256caa11af294979c1f09
estebancqe <estebancqe@gmail.com>
2025-03-09 02:12:26 -0500

    Update swiper_state.py

--- Creyente/state/swiper_state.py
+++ Creyente/state/swiper_state.py
@@ -1,38 +1,41 @@
 import reflex as rx
-class SwiperState1(rx.State):
+
+class SwiperState(rx.State):
     current_slide: int = 1
     total_slides: int = 5
 
-    @rx.event
-    def init_swiper (self):
+    @rx.event(background=True)
+    async def init_swiper(self):
         return rx.call_script(
             """
-            new Swiper('.swiper-container', {
-                slidesPerView: 1,
-                spaceBetween: 20,
-                loop: true,
-                pagination: {
-                    el: '.swiper-pagination',
-                    clickable: true
-                },
-                navigation: {
-                    nextEl: '.swiper-button-next',
-                    prevEl: '.swiper-button-prev'
-                },
-                on: {
-                    slideChange: function () {
-                        document.querySelector('.mobile-indicator').textContent = 
-                        `${this.realIndex + 1}/${this.slides.length}`;
-                    }
-                },
-                breakpoints: {
-                    640: {
-                        slidesPerView: 2
+            if (typeof Swiper !== 'undefined') {
+                new Swiper('.swiper-container', {
+                    slidesPerView: 1,
+                    spaceBetween: 20,
+                    loop: true,
+                    pagination: {
+                        el: '.swiper-pagination',
+                        clickable: true
                     },
-                    1024: {
-                        slidesPerView: 2
+                    navigation: {
+                        nextEl: '.swiper-button-next',
+                        prevEl: '.swiper-button-prev'
+                    },
+                    on: {
+                        slideChange: function () {
+                            document.querySelector('.mobile-indicator').textContent = 
+                            `${this.realIndex + 1}/${this.slides.length}`;
+                        }
+                    },
+                    breakpoints: {
+                        640: {
+                            slidesPerView: 2
+                        },
+                        1024: {
+                            slidesPerView: 2
+                        }
                     }
-                }
-            });
+                });
+            }
             """
         )
