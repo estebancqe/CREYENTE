8f123d18ddbf011f147d089ee06a07633bcccbbb
estebancqe <estebancqe@gmail.com>
2025-03-27 22:50:30 -0500

    cambio de render a railway

--- rxconfig.py
+++ rxconfig.py
@@ -7,7 +7,7 @@ config = AppConfig(
     app_name="Creyente",
     db_url="sqlite:///reflex.db",
     env=rx.Env.DEV,
-    api_url="https://creyente.onrender.com",
+    api_url="creyente-production.up.railway.app",
     cors_allowed_origins=[
         "http://localhost:3000",
         "https://creyente.vercel.app",
