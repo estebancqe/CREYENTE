e40ddc266168041d9259c6b5f1cb16c8058568d5
estebancqe <estebancqe@gmail.com>
2025-03-30 21:41:39 -0500

    version 7.0.2 reflex

--- rxconfig.py
+++ rxconfig.py
@@ -1,17 +1,16 @@
 import reflex as rx
 
-config = rx.Config(
+class AppConfig(rx.Config):
+    pass
+
+config = AppConfig(
     app_name="Creyente",
-    # Para producción, usa una base de datos PostgreSQL
-    db_url="postgresql://user:password@localhost:5432/creyente_db",  
+    db_url="sqlite:///reflex.db",
+    env=rx.Env.DEV,
     api_url="https://creyente.onrender.com",
-    # Asegúrate que los CORS estén correctamente configurados
     cors_allowed_origins=[
         "http://localhost:3000",
         "https://creyente.vercel.app",
-        "https://jcqsoft.com/creyente"
+        "https://jcqsoft.com/creyente",
     ],
-    # Configuración para producción
-    frontend_port=3000,
-    backend_port=8000
 )
