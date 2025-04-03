3d86449c712365ad0beffaa90ea3c7a9d2e0f504
estebancqe <estebancqe@gmail.com>
2025-04-02 19:09:08 -0500

    Update remote_build.sh

--- remote_build.sh
+++ remote_build.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 cd Documents/Creyente
 python -m venv .venvcreyente
 source .venvcreyente/bin/activate
@@ -5,7 +6,7 @@ pip install --upgrade pip
 pip install -r requirements.txt 
 rm -rf public
 reflex init
-API_URL=https://creyente.onrender.com reflex export --frontend-only --no-zip [(1)](https://reflex.dev/docs/hosting/self-hosting)
+reflex export --frontend-only --no-zip
 mkdir -p public
-cp -r .web/_static/* public/ [(1)](https://reflex.dev/docs/hosting/self-hosting)
+cp -r .web/_static/* public/
 deactivate
