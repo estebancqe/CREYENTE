f00be63b4b7be4e93dc3cefb791c9a87406d4b5d
estebancqe <estebancqe@gmail.com>
2025-04-03 14:35:51 -0500

    Update rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -3,7 +3,7 @@ import reflex as rx
 config = rx.Config(
     app_name="Creyente",
     db_url="sqlite:///reflex.db",
-    api_url="https://creyente.onrender.com",
+    api_url="https://creyente-web.up.railway.app",
     cors_allowed_origins=[
         "http://localhost:3000", 
         "https://creyente.vercel.app",
