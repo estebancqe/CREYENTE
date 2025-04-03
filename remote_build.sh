d3f8ab9ab0a168210107d2717490b84085a7449c
estebancqe <estebancqe@gmail.com>
2025-04-02 19:06:18 -0500

    Update remote_build.sh

--- remote_build.sh
+++ remote_build.sh
@@ -5,7 +5,7 @@ pip install --upgrade pip
 pip install -r requirements.txt 
 rm -rf public
 reflex init
-API_URL=https://creyente.onrender.com reflex export --frontend-only
-unzip frontend.zip -d public
-rm -f frontend.zip
+API_URL=https://creyente.onrender.com reflex export --frontend-only --no-zip [(1)](https://reflex.dev/docs/hosting/self-hosting)
+mkdir -p public
+cp -r .web/_static/* public/ [(1)](https://reflex.dev/docs/hosting/self-hosting)
 deactivate
